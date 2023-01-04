e2d6d8441f21a6ce657e3e7209a36f71928604b9
jhoves <2332689115@qq.com>
2023-01-04 11:27:20 +0800

    初始化项目

---
+++ derliderli/derliderli-api/src/main/java/com/jhoves/derliderliApplication.java
@@ -0,0 +1,15 @@
+package com.jhoves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JHoves
+ * @create 2023-01-04 11:04
+ */
+@SpringBootApplication
+public class derliderliApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(derliderliApplication.class,args);
+    }
+}
